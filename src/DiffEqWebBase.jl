2674355d42479f4ff125fb4ed5b5d6d1ee7929d1
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-15 00:31:56 -0800

    Base problem/solve

--- src/DiffEqWebBase.jl
+++ src/DiffEqWebBase.jl
@@ -1,5 +1,34 @@
+__precompile__()
+
 module DiffEqWebBase
+  using OrdinaryDiffEq, DiffEqBase
 
-# package code goes here
+  import DiffEqBase: build_solution
 
-end # module
+  immutable QuickODEProblem{uType,tType,isinplace} <: AbstractODEProblem{uType,tType,isinplace,Function}
+    f::Function
+    u0::uType
+    tspan::Tuple{tType,tType}
+  end
+
+  immutable QuickODESolution{uType,tType,rateType,P,A} <: AbstractODESolution
+    u::uType
+    t::tType
+    k::rateType
+    prob::P
+    alg::A
+    interp::OrdinaryDiffEq.InterpolationData
+    dense::Bool
+    tslocation::Int
+  end
+  (sol::QuickODESolution)(t) = sol.interp(t)
+
+  function build_solution{uType,tType,isinplace}(
+          prob::QuickODEProblem{uType,tType,isinplace},
+          alg,t,u;dense=false,
+          k=[],interp = (tvals) -> nothing,kwargs...)
+    QuickODESolution(u,t,k,prob,alg,interp,dense,0)
+  end
+
+  export QuickODEProblem, QuickODESolution
+end
